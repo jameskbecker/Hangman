d3b17e67d23ebb80c0f71d9a7339ecaa3684d9c2
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-03-05 14:10:03 +0000

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by James Becker on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
